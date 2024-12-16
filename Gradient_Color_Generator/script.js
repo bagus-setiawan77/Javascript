559b246864391183d34c5c2caec4e50407fa434b
bagus-setiawan77 <bagus.setiawan.117788@gmail.com>
2024-12-16 16:00:12 +0700

    update/javascript/html-css-javascript

--- Gradient_Color_Generator/script.js
+++ Gradient_Color_Generator/script.js
@@ -1,2 +1,9 @@
+const gradientBox = document.getElementById('gradientBox');
+const selectMenu = document.getElementById('selectMenu');
 
+const getRandomColor = () =>{
+    const randomHex = Math.floor(Math.random() * 0xffffff).toString(16);
+    return `#${randomHex}`
+}
 
+alert(getRandomColor)
